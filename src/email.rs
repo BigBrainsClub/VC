6e8db91d844638a0c0192a94ead9ce6c2d678246
molodost-vnutri <90547216+molodost-vnutri@users.noreply.github.com>
2025-02-28 06:42:29 +0300

    Оптимизация email/login функций

--- src/email.rs
+++ src/email.rs
@@ -1,109 +1,75 @@
 #[inline(always)]
 pub fn is_valid_email(email: &[u8]) -> bool {
-    let len: i8 = match email.len().try_into() {
-        Ok(len) => len,
-        _ => return false,
-    };
-
+    let len = email.len();
     if len <= 5 {
         return false;
     }
 
     unsafe {
-        let ptr: *const u8 = email.as_ptr();
+        let ptr = email.as_ptr();
         let mut at_pos: i8 = -1;
         let mut dot_pos: i8 = -1;
-        let mut is_domain: bool = false;
         let mut last_char_was_dash = false;
 
-        for i in 0..len {
-            let c: u8 = *ptr.add(i as usize);
+        for i in 0..len as i8 {
+            let c = *ptr.add(i as usize);
 
-            if c == b'@' {
-                if i > 0 && *ptr.add(i as usize - 1) == b'.' {
-                    return false;
+            match c {
+                b'@' => {
+                    if at_pos != -1 || i == 0 || *ptr.add(i as usize - 1) == b'.' {
+                        return false;
+                    }
+                    at_pos = i;
+                    if i + 1 >= len as i8 {
+                        return false;
+                    }
+                    let next_c = *ptr.add(i as usize + 1);
+                    if !next_c.is_ascii_alphanumeric() {
+                        return false;
+                    }
                 }
-
-                is_domain = true;
-                if at_pos != -1 {
-                    return false;
+                b'.' => {
+                    if i == 0 || i == len as i8 - 1 || (at_pos != -1 && i == at_pos + 1) {
+                        return false;
+                    }
+                    dot_pos = i;
+                    last_char_was_dash = false;
                 }
-
-                let next_ptr = *ptr.add(i as usize + 1);
-                if !(
-                    (next_ptr >= b'a' && next_ptr <= b'z')
-                        || (next_ptr >= b'A' && next_ptr <= b'Z')
-                        || (next_ptr >= b'0' && next_ptr <= b'9')
-                ) {
-                    return false;
-                }
-
-                at_pos = i;
-            } else if c == b'.' {
-                dot_pos = i;
-                last_char_was_dash = false;
-            } else if c <= b' ' {
-                return false;
-            } else if !(
-                (c >= b'a' && c <= b'z')
-                    || (c >= b'A' && c <= b'Z')
-                    || (c >= b'0' && c <= b'9')
-                    || c == b'-'
-            ) && !is_domain && *ptr.add(1) == b'@'
-            {
-                return false;
-            }
-
-            if is_domain {
-                if i == at_pos + 1 && c == b'.' {
-                    return false;
-                }
-                if i == len - 1 && c == b'.' {
-                    return false;
-                }
-                if c == b'-' {
-                    if i == at_pos + 1 || *ptr.add(i as usize - 1) == b'.' || *ptr.add(i as usize + 1) == b'.' {
+                b'-' => {
+                    if i == 0 || i == len as i8 - 1 || *ptr.add(i as usize - 1) == b'.' || *ptr.add(i as usize + 1) == b'.' {
                         return false;
                     }
                     if last_char_was_dash {
                         return false;
                     }
                     last_char_was_dash = true;
-                } else {
-                    last_char_was_dash = false;
                 }
+                0..=32 => return false,
+                _ => last_char_was_dash = false,
             }
         }
 
-        if at_pos == -1 || dot_pos == -1 {
+        if at_pos == -1 || dot_pos <= at_pos + 1 {
             return false;
         }
 
-        let domain_part = &email[(at_pos + 1) as usize..];
-        let domain_parts: Vec<&[u8]> = domain_part.split(|&x| x == b'.').collect();
+        let mut part_len = 0;
+        let mut domain_parts = 0;
 
-        if domain_parts.is_empty() {
-            return false;
-        }
-
-        if let Some(last_part) = domain_parts.last() {
-            let after_dot_len = last_part.len();
-            if after_dot_len < 2 || after_dot_len > 3 {
-                return false;
+        for i in at_pos as usize + 1..len {
+            let c = *ptr.add(i);
+            if c == b'.' {
+                if part_len == 0 {
+                    return false;
+                }
+                domain_parts += 1;
+                part_len = 0;
+            } else {
+                part_len += 1;
             }
         }
 
-        if domain_part.last() == Some(&b'.') {
-            return false;
-        }
-
-        for part in domain_parts {
-            if part.is_empty() || !part.iter().all(|&c| (c >= b'a' && c <= b'z') || (c >= b'A' && c <= b'Z') || (c >= b'0' && c <= b'9') || c == b'-') {
-                return false;
-            }
-        }
-
-        if at_pos == 0 || dot_pos <= at_pos + 1 || dot_pos == (len - 1) {
+        if domain_parts == 0 || part_len < 2 || part_len > 3 {
             return false;
         }
     }
--- src/login.rs
+++ src/login.rs
@@ -1,41 +1,32 @@
 #[inline(always)]
 pub fn is_valid_login(login: &[u8]) -> bool {
-    let len: i8 = match login.len().try_into() {
-        Ok(len) => len,
-        _ => return false
-    };
+    let len = login.len();
     if len < 3 {
         return false;
     }
 
     unsafe {
-        let mut ptr: *const u8 = login.as_ptr();
-        let end: *const u8 = ptr.add(len as usize);
+        let mut ptr = login.as_ptr();
+        let first = *ptr;
+        if !first.is_ascii_alphanumeric() {
+            return false;
+        }
 
-        let first: u8 = *ptr;
-        if !(
-                (first >= b'a' && first <=b'z') ||
-                (first >= b'A' && first <= b'Z') ||
-                (first >= b'0' && first <= b'9')
-            ) {
+        let mut prev = first;
+        ptr = ptr.add(1);
+        let last_ptr = ptr.add(len - 1);
+
+        while ptr <= last_ptr {
+            let b = *ptr;
+
+            if !(b.is_ascii_alphanumeric() || b == b'_' || b == b'-' || b == b'.') {
                 return false;
             }
-        let mut prev: u8 = first;
-        ptr = ptr.add(1);
 
-        while ptr < end {
-            let b: u8 = *ptr;
-            if !(
-                    (b >= b'a' && b <= b'z') ||
-                    (b >= b'A' && b <= b'Z') ||
-                    (b >= b'0' && b <= b'9') ||
-                    b == b'_' || b == b'-' || b == b'.'
-                ) {
-                    return false;
-                }
             if (b == b'.' || b == b'-' || b == b'_') && prev == b {
                 return false;
             }
+
             prev = b;
             ptr = ptr.add(1);
         }
